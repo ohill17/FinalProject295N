a32894b4ca9fe1835cca79eff0e56c900cdc54ea
Orion Hill <hillo@my.lanecc.edu>
2023-11-27 11:19:17 -0800

    Initial Commit with VS files

---
+++ FinalProject/Controllers/ConcentrationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Controllers
+{
+    public class ConcentrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
